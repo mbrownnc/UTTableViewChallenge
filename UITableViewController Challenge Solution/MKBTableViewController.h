447d9a5ae1c0fdd69cb5b3656ba41d30ab60e671
Matt Brown <Decoy@Matt-MBA.local>
2014-05-26 20:04:44 -0500

    first commit

---
+++ UITableViewController Challenge Solution/MKBTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MKBTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Matt Brown on 5/26/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKBTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *cellText;
+
+@end
